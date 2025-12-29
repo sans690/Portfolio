b0f54e17cc99cfcec40c421a314057bd5053f88c
sans690 <scsmith7255@gmail.com>
2025-12-29 14:31:18 -0500

    initial file setup

---
+++ portfolio/src/ui/Contact.tsx
@@ -0,0 +1,16 @@
+
+function Contact() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Title</h1>
+      <div className="card">
+        <a href='/home/'>Home</a>
+      </div>
+    </>
+  )
+}
+
+export default Contact
