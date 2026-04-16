80550474272de52f281de6048ea6e844e4836425
sans690 <scsmith7255@gmail.com>
2026-04-16 11:49:43 -0400

    Centering of boxes and text

--- components/src/About.tsx
+++ components/src/About.tsx
@@ -14,52 +14,58 @@ export function About() {
             <p className="text">
               Here are a few things about me that might catch your interest
             </p>
-            <div className="card_container">
+            <div className="card_container ">
               {/* 1st box */}
               <div className="about_border_card">
                 {" "}
-                <p className="label text-left text-3xl mb-5">Education</p>
-                <p className="text_in_box text-left">University of Virginia</p>
-                <p className="text_in_box text-left">
-                  Central Virginia Community College
-                </p>
-                <p className="text_in_box text-left">
-                  Blue Ridge Community College-SDL
-                </p>
-                <p className="text_in_box text-left">
-                  Virginia Western Community College-SDL
-                </p>
+                <p className="label text-center text-3xl mb-5">Education</p>
+                <ul className="list-disc space-y-2 pl-6 text-left marker:text-purple-300">
+                  <li className="text_in_box">University of Virginia</li>
+                  <li className="text_in_box">
+                    Central Virginia Community College
+                  </li>
+                  <li className="text_in_box">
+                    Blue Ridge Community College-SDL
+                  </li>
+                  <li className="text_in_box">
+                    Virginia Western Community College-SDL
+                  </li>
+                </ul>
               </div>
               {/* 2nd box */}
               <div className="about_border_card">
                 {" "}
-                <p className="label text-left text-3xl mb-5">
+                <p className="label text-center text-3xl mb-5">
                   Experiences
                 </p>{" "}
-                <p className="text_in_box text-left">
-                  CloudFit Software: Software Development Intern
-                </p>
-                <p className="text_in_box text-left">
-                  Radiant Health and Wellness: Front Desk/ IT
-                </p>
-                <p className="text_in_box text-left">
-                  Central Virginia Community College: Peer Writing Tutor
-                </p>
+                <ul className="list-disc space-y-2 pl-6 text-left marker:text-purple-300">
+                  <li className="text_in_box">
+                    CloudFit Software: Software Development Intern
+                  </li>
+                  <li className="text_in_box">
+                    Radiant Health and Wellness: Front Desk/ IT
+                  </li>
+                  <li className="text_in_box">
+                    Central Virginia Community College: Peer Writing Tutor
+                  </li>
+                </ul>
               </div>
               {/* 3rd box */}
               <div className="about_border_card">
-                <p className="label text-left text-3xl mb-5">Interests</p>
-                <p className="text text-left">
-                  <p className="text_in_box text-left">Welding</p>
-                  <p className="text_in_box text-left">3D Printing</p>
-                  <p className="text_in_box text-left">Woodworking</p>
-                  <p className="text_in_box text-left">Art</p>
-                </p>
+                <p className="label text-center text-3xl mb-5">Interests</p>
+                <ul className="list-disc space-y-2 pl-6 text-left marker:text-purple-300">
+                  <li className="text_in_box">Welding</li>
+                  <li className="text_in_box">3D Printing</li>
+                  <li className="text_in_box">Woodworking</li>
+                  <li className="text_in_box">Art</li>
+                </ul>
               </div>
             </div>
             <div className="flex flex-col mt-20 justify-center items-center">
               <div className="w-full h-full min-h-5px max-w-7xl bg-linear-to-br from-purple-950/30 to-black p-8 rounded-xl border border-purple-600/30 ">
-                <p className="label text-left text-3xl mb-6">Recommendations</p>
+                <p className="label text-center text-3xl mb-6">
+                  Recommendations
+                </p>
                 <p className="text text-left">
                   <p className="text_in_box text-left whitespace-pre-line">
                     {ChrisRec}
